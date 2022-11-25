d42c5433d49982f58c68a757390e05961714dda1
Mike Loseto <losetom1998@gmail.com>
2022-11-25 10:29:07 -0500

    Rails Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
-end
+    include ActionController::Cookies
+  
+    def hello_world
+      session[:count] = (session[:count] || 0) + 1
+      render json: { count: session[:count] }
+    end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: "John Smith")
+User.create!(name: "Sally Jones")
+User.create!(name: "D'ante Hightower")
+User.create!(name: "Carlos Santana")
