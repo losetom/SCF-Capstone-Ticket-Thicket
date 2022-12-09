dcbd4fc4cb3f87cc657316c654b736fe4e60536c
Mike Loseto <losetom1998@gmail.com>
2022-12-08 20:43:05 -0500

    Added Tickets CRUD

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,45 @@ class TicketsController < ApplicationController
     def index
         render json: Ticket.all
     end
+
+    def create
+        ticket = Ticket.create!(ticket_params)
+        render json: ticket, status: :created
+    end
+
+    def update
+        ticket = Ticket.find_by(id: params[:id])
+        if ticket
+            ticket.update(ticket_params)
+            render json: ticket
+        else
+            render json: { error: "Ticket not found" }, status: :not_found
+        end
+    end
+
+    def show
+        ticket = Ticket.find_by(id: params[:id])
+        if ticket
+            render json: ticket
+        else
+            render json: { error: "Ticket not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        ticket = Ticket.find_by(id: params[:id])
+        if ticket 
+            ticket.destroy
+            head :no_content
+        else
+            render json: { error: "Ticket not found" }, status: :not_found
+        end
+
+    end
+
+    private
+
+    def ticket_params
+        params.permit(:name, :address, :city, :state, :zip, :seat, :time)
+    end
 end
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,7 @@ function LoginForm() {
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Email address..." />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -15,10 +15,10 @@ function LoginForm() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password..." />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check type="checkbox" label="Remember Me" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
